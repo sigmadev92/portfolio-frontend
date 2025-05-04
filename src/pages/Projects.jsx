5f8673288bbf371153cb3ebd901fb62f9e00c2fd
sigmadev92 <devanshofficial2000@gmail.com>
2025-05-04 09:59:55 +0530

    categorized projects page.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,131 @@
 import React from "react";
-
+import "../components/ProjectPage/projectpage.css";
+import { FaFolderOpen } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 function Projects() {
+  const projectsInfo = [
+    {
+      heading: "Web Development",
+      keyword: "web-dev",
+      subCategory: true,
+      content: [
+        {
+          heading: "Frontend",
+          keyword: "frontend",
+          content: [
+            { link: "/html-css", name: "HTML+CSS" },
+            { link: "/css-shapes", name: "CSS Shapes" },
+            { link: "/css-animations", name: "CSS Animations" },
+            { link: "/js", name: "JavaScript" },
+            { link: "/react", name: "ReactJS" },
+          ],
+        },
+        {
+          heading: "Full Stack",
+          keyword: "full-stack",
+          content: [
+            { link: "/mern", name: "MERN" },
+            { link: "/embededjs", name: "EmbededJS" },
+            { link: "/django", name: "Django+React" },
+            { link: "/spring-boot", name: "SpringBoot + React" },
+            { link: "/laravel", name: "Laravel + React" },
+          ],
+        },
+      ],
+    },
+    {
+      heading: "Desktop Applications",
+      keyword: "desktop",
+      content: [
+        { link: "/electron-node", name: "Electron+NodeJS" },
+        { link: "/java-gui", name: "Java + JavaFx" },
+        { link: "/cpp-gui", name: "C++ GUI" },
+        { link: "/c#-wpf", name: "C#+WPF" },
+        { link: "/python-gui", name: "Python GUI" },
+      ],
+    },
+    {
+      heading: "3d Modelling and Animations",
+      keyword: "3d-modelling",
+      content: [{ link: "/blender", name: "Blender" }],
+    },
+    {
+      heading: "Game Development",
+      keyword: "game-dev",
+      content: [
+        { link: "/unity", name: "C# + Unity" },
+        { link: "/browser", name: "JS + Browser" },
+      ],
+    },
+    {
+      heading: "Data Science, AI and ML",
+      keyword: "data-science",
+      content: [
+        { link: "/ms-excel", name: "MS Excel" },
+        { link: "/powerbi", name: "Power BI" },
+        { link: "/python", name: "Python" },
+      ],
+    },
+  ];
+
+  const navigate = useNavigate();
   return (
-    <div className="h-[100vh]">
-      <h2>My Projects</h2>
+    <div id="projectpage-div" className="bg-black text-white justify-center">
+      {projectsInfo.map((box, indexOuter) => {
+        return (
+          <div
+            className="projectpage-divs"
+            id={`projectpage-${box.keyword}`}
+            key={indexOuter}
+          >
+            <h2 className="box-heading">{box.heading}</h2>
+            {box.subCategory ? (
+              box.content.map((part, indexMiddle) => {
+                return (
+                  <>
+                    <h3 className="box-subheading">{part.heading}</h3>
+                    <div className="folder-container" key={indexMiddle}>
+                      {part.content.map((skill, indexInner) => {
+                        return (
+                          <div
+                            className="folder"
+                            key={indexInner}
+                            onClick={() =>
+                              navigate(
+                                `/${box.keyword}/${part.keyword}${skill.link}`
+                              )
+                            }
+                          >
+                            <FaFolderOpen className="folder-icon" />
+                            <span className="folder-name truncate">
+                              {skill.name}
+                            </span>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </>
+                );
+              })
+            ) : (
+              <div className="folder-container">
+                {box.content.map((skill, indexInner) => {
+                  return (
+                    <div
+                      className="folder"
+                      key={indexInner}
+                      onClick={() => navigate(`/${box.keyword}${skill.link}`)}
+                    >
+                      <FaFolderOpen className="folder-icon" />
+                      <span className="folder-name">{skill.name}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/pages/SummaryPage.jsx
+++ src/pages/SummaryPage.jsx
@@ -1,5 +1,43 @@
 import React from "react";
 
 export default function SummaryPage() {
-  return <div className="min-h-[100vh]">SummaryPage</div>;
+  return (
+    <div className="min-h-[100vh] w-[90%] md:w-[60%] m-auto">
+      <p>
+        <span className="text-blue-500">Hello</span>, My name is{" "}
+        <strong>Devansh Raghuwanshi</strong>. <br /> <br />I completed my school
+        education at <i>Delhi Public School Vijaypur in 2019 </i>
+        in <b>science Stream</b>. <br />
+        My interest developed to solve complex mathematical problems and I
+        decided to study science in depth and took admission in{" "}
+        <strong>
+          <span className="text-red-500">BSc H Mathematics</span> at{" "}
+          <span className="text-blue-500">
+            Jaypee University of Engineering and Technology Guna
+          </span>{" "}
+          in <u>2019</u>.
+        </strong>
+        <br />
+        That was the best decision of my life as I learnt and understood so many
+        things. Though I was a student of Mathematics but stil the University
+        provided me with all possible disciplines like Computer Science,
+        Mechanics and Physics.
+        <br />
+        Till my final year, I gained experience in many technologies of computer
+        science like making websites and working on different languages like
+        Python, C++, Matlab etc. Also they taught me essential concepts like DSA
+        and DBMS
+        <br />
+        I graduated in 2022 and for 2 years I experimented and learned so many
+        things like multimedia editing, 3d modelling and animations. I prepared
+        for exams like SBI PO, SSC CGL and MPPSC. Unfortunately I couldn't crack
+        those exams but gained much knowledge in solving aptitude and GS
+        questions. I later taught many students Quants and Reasoning.
+        <br />
+        Now from Feb 2025 onwards, I decided to continue my career in Software
+        development field. I have made web Apps, desktop apps, animations and 3d
+        modelling and have a keen interest in Data Science as well.
+      </p>
+    </div>
+  );
 }
